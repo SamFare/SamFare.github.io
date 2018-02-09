9ba153ca6e964e858d6e6d015504cbf79415b1d9
SamFare <Samuel.fare@bglgroup.co.uk>
2018-02-09 22:22:45 +0000

    Added new nav bar to allow navigation of site

--- src/NavigationBar/NavigationBar.js
+++ src/NavigationBar/NavigationBar.js
@@ -1,28 +1,27 @@
 import React from "react";
 import { Nav, NavItem, Navbar } from "react-bootstrap";
 
-//import css from "./NavigationBar.css";
+
+import css from "./NavigationBar.css";
 
 export default class NavigationBar extends React.Component{
   render(){
     return (
-      <Navbar>
-        <Navbar.Header>
-          <Nav>
-            <NavItem href="#">
-              Home
-            </NavItem>
-            <NavItem href="#">
-              About
-            </NavItem>
-            <NavItem href="#">
-              Experence
-            </NavItem>
-            <NavItem href="#">
-              Open Source
-            </NavItem>
-          </Nav>
-        </Navbar.Header>
+      <Navbar className={"col-md-24"}>
+        <ol className={`${css.centreNav}`}>
+          <li className={css.navItem}>
+            <a href="#"> About </a>
+          </li>
+          <li className={css.navItem}>
+            <a href="#"> Experence </a>
+          </li>
+          <li className={css.navItem}>
+            <a href="https://github.com/SamFare"> GitHub </a>
+          </li>
+          <li className={css.navItem}>
+            <a href="https://medium.com/@samuel.fare"> Medium </a>
+          </li>
+        </ol>
       </Navbar>
     );
   }
