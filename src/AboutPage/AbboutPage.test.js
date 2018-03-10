28ee7883d516d85cac5a7919cd3fefa94aae87db
SamFare <Samuel.fare@bglgroup.co.uk>
2018-03-10 23:54:15 +0000

    Added AboutPage test outline

---
+++ src/AboutPage/AbboutPage.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ListOfTextElement from "./ListOfTextElement.js";
+import renderer from "react-test-renderer";
+
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+let context = {};
+
+describe("AboutPage", () => {
+
+});
