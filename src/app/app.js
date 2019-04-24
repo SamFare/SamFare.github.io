b5952852c8f503534359ff78008abb0dd96c5170
Samuel Fare <samuel.fare@gmail.com>
2019-04-24 22:49:34 +0100

    Removed now unused nav components

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TitleBar from "../TitleBar/titleBar.js";
+import Nav from "../Nav/Nav.js";
 import AboutPage from "../AboutPage/AboutPage.js";
 import Footer from "../Footer/Footer.js";
 
+import navConfig from "../../data/config/config.json";
 class App extends React.Component{
   render(){
     return (
       <div>
-        <TitleBar />
+        <Nav logo={navConfig.logo} options={navConfig.options}></Nav>
         <AboutPage />
         <Footer />
       </div>
