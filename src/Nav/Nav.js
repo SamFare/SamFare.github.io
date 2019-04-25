599498218f9ac4c576f08ab8c9ba4bcfa7bef14e
Samuel Fare <samuel.fare@gmail.com>
2019-04-25 13:37:16 +0100

    refactor of Nav.js

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,21 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import css from "./Nav.css";
 
+const buildOptionMenu = (options) => { 
+  return (<ul className={css.menu}>
+    {
+      options.map(option => { 
+        return <li key={option.key} className={`${css.navButton} qa-option`}>{option.value}</li>;
+      })
+    }
+  </ul> );
+};
 
 const Nav = (props) => { 
   return(
     <div className={`${css.nav} qa-nav`}>
-      {
-        props.logo? <img src={props.logo} className={`${css.logo} qa-logo`}></img> : <noscript />
-        
-      }
-      <ul className={css.menu}>
-        {
-          props.options.map(option => { 
-            return <li key={option.key} className={`${css.navButton} qa-option`}>{option.value}</li>;
-          })
-        }
-      </ul> 
+      { props.logo? <img src={props.logo} className={`${css.logo} qa-logo`}></img> : <noscript />}
+      {buildOptionMenu(props.options)}
     </div>
   );
 };
