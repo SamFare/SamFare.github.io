ce767fbdd8d1e1fbfd165405e575687c62094fda
Samuel Fare <samuel.fare@gmail.com>
2019-04-28 19:31:52 +0100

    added my background to main scerrn

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -2,14 +2,21 @@ import React from "react";
 
 import PhotoHeader from "../PhotoHeader/PhotoHeader.js";
 import ListOfTextElement from "../ListOfTextElement/ListOfTextElement.js";
-import Paragraph from "../Paragraph/Paragraph.js";
 import Subheading from "../Subheading/Subheading.js";
 
 import careerData from "../../data/career-data.json";
 import activityData from "../../data/activity-data.json";
-import aboutData from "../../data/about-data.json";
+import ImageList from "../ImageList/image-list.js";
 
 import css from "./AboutPage.css";
+
+const images = [
+  {url: "assets/bbc.png"},
+  {url: "assets/ge.png"},
+  {url: "assets/philips.png"},
+  {url: "assets/ctm.png"}, 
+
+];
 
 export default class AboutPage extends React.Component{
   render(){
@@ -17,8 +24,8 @@ export default class AboutPage extends React.Component{
       <div>
         <PhotoHeader arrowImgUrl={"assets/down-arrow.png"} text={"Modern, Clean, High Quality Software Engineering For People First Organisations"} mobileImgUrl={"assets/photo-header-background-mobile.png"} imgUrl={"assets/photo-header-background.png"}/>
         <div className={css.centerDiv}>
-          <Subheading text="About"/>
-          <Paragraph text={aboutData.data}/>
+          <Subheading text="My Background"/>
+          <ImageList images={images} />
           <Subheading text="What I'm up too"/>
           <ListOfTextElement elements={activityData.data} />
           <Subheading text="Experience"/>
