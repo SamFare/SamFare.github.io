2813a9861bd9a8c260532e69edea947c8a8ab21e
Samuel Fare <samuel.fare@gmail.com>
2019-06-02 22:26:59 +0100

    Added about section

--- src/AboutPage/AbboutPage.test.js
+++ src/AboutPage/AbboutPage.test.js
@@ -1,22 +1,9 @@
 import React from "react";
 import AboutPage from "./AboutPage.js";
-import renderer from "react-test-renderer";
+import { mount } from "enzyme";
 
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 
-Enzyme.configure({ adapter: new Adapter() });
-
-let context = {};
-
-describe("an about page", () => {
-  beforeEach(() => {
-    context.aboutPage = renderer.create(
-      <AboutPage />
-    );
-  });
-
-  it("it has the image that was passed in", () => {
-    expect(context.aboutPage).toMatchSnapshot();
-  });
+describe("The about page displays a paragraph box", () => { 
+  const wrapper = mount(<AboutPage />);
+  expect(wrapper.find(".qa-paragraph").length).toEqual(0);
 });
